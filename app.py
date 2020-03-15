f81b59ddd5f3f607a8a36d1f30e9fd54f4b530bf
Liz94688 <elizabeth.stephens@hotmail.co.uk>
2020-03-15 16:05:07 +0000

    Added templates folder and plants.html file

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'test_forFlourish'
+app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@myfirstcluster-suguw.mongodb.net/test_forFlourish?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
+
 
 @app.route('/')
-def hello():
-    return 'Hello World'
+@app.route('/get_plants')
+def get_plants():
+    return render_template("plants.html", plants=mongo.db.plants.find())
 
 
 if __name__ == '__main__':

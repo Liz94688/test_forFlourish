f83bd7c8fbd741b391ffe6f2fcd27f793d647090
Liz94688 <elizabeth.stephens@hotmail.co.uk>
2020-03-29 13:42:55 +0000

    Imported add modules and started form class and reg route

--- app.py
+++ app.py
@@ -1,45 +1,76 @@
+# Importing Modules
 import os
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, flash, redirect, request, url_for, session, logging
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from wtforms import Form, BooleanField, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
 
+# Declaring App Name
 app = Flask(__name__)
 
+# Configure Setting and Environmental Variables
 app.config["MONGO_DBNAME"] = 'test_forFlourish'
 app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@myfirstcluster-suguw.mongodb.net/test_forFlourish?retryWrites=true&w=majority'
 
 mongo = PyMongo(app)
 
-
+# Welcome Page
 @app.route('/')
 @app.route('/welcome_page')
 def welcome_page():
     return render_template("welcome_page.html")
 
 
-@app.route('/register_user')
+# Form Class
+class RegisterForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    email = StringField('Email', [validators.Length(min=6, max=50)])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords do not match')
+    ])
+    confirm = PasswordField('Confirm Password')
+
+# Register
+@app.route('/register_user', methods=['GET', 'POST'])
 def register_user():
-    return render_template("register_user.html")
+    # Create Form Variable
+    form = RegisterForm(request.form)
+    # Check if POST or GET reguest
+    if request.method == 'POST' and form.validate():
+        return render_template("register_user.html")
+        # Register New User
+    return render_template("register_user.html", form=form)
 
-
+# Login
 @app.route('/login_user')
 def login_user():
     return render_template("login_user.html")
 
+# User Account
+@app.route('/user_account')
+def user_account():
+    return render_template("user_account.html")
 
-@app.route('/user_record')
-def user_record():
-    return render_template("user_record.html")
-
-
+# Search Plants
 @app.route('/search_plants')
 def search_plants():
     return render_template("search_plants.html", plants=mongo.db.plants.find())
 
+# Plant Record
+@app.route('/plant_record')
+def plant_record():
+    return render_template("plant_record.html")
 
-@app.route('/edit_record')
-def edit_record():
-    return render_template("edit_record.html")
+# Edit User Plant Record
+@app.route('/edit_user_plant_record')
+def edit_user_plant_record():
+    return render_template("edit_user_plant_record.html")
+
+# Delete User Plant Record
+# Log Out
 
 
 if __name__ == '__main__':

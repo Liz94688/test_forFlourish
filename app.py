e9426228c1d9d8bf0abfc6d3e20d3351d3537b2b
Liz94688 <elizabeth.stephens@hotmail.co.uk>
2020-03-31 17:52:36 +0000

    Started login and sessions, included decorator to check if user logged in

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 # Importing Modules
 import os
-from flask import Flask, render_template, flash, redirect, request, url_for, session, logging
+import logging
+from flask import (
+    Flask, render_template, flash, redirect, request, url_for, session)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from wtforms import Form, BooleanField, StringField, TextAreaField, PasswordField, validators
+from functools import wraps
 
 # Declaring App Name
 app = Flask(__name__)
@@ -11,6 +14,7 @@ app = Flask(__name__)
 # Configure MongoDB
 app.config["MONGO_DBNAME"] = 'test_forFlourish'
 app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@myfirstcluster-suguw.mongodb.net/test_forFlourish?retryWrites=true&w=majority'
+app.config['SECRET_KEY'] = 'florish123'
 
 mongo = PyMongo(app)
 
@@ -38,9 +42,6 @@ def register_user():
     form = RegisterForm(request.form)
     users = mongo.db.users
     if request.method == 'POST' and form.validate():
-        # Start a session using username
-        # session ['username'] = request.form['username']
-
         # Insert input fields into MongoDB users collection
         users.insert({
             'name': request.form['name'].lower(),
@@ -51,70 +52,83 @@ def register_user():
         # Message to acknowledge registration needed
         # flash('Registration complete. Please log in.', 'success')
 
-        # End session
-        # session.pop('usernam', None)
         return redirect(url_for('login_user'))
     return render_template('register_user.html', form=form)
 
-# Login - URL_FOR doesn't work...
+# Login - POST works but doesn't re-direct, as expected
 @app.route('/login_user', methods=['GET', 'POST'])
 def login_user():
     users = mongo.db.users
     if request.method == 'POST':
-        # Start a session using username
-        # session ['username'] = request.form['username']
+        # Get form fields
+        username_input = request.form['username']
+        # password_input = request.form['password']
 
-        # Get fields from form - need to include a password request
-        username = request.form['username']
-        # password_input = request.form['password'] - need to research passwords
-
-        # Get user from DB using username or password_input
-        result = users.find('username')
-
+        # Check username exists in database
+        user_result = users.find({
+            'username': request.form['username'].lower()
+        })
         # Check results
-        if result > 0:
-            # Compare username in form field to username from DB
-            if username == result:
-                logging.info('Usernames matched')
-                # Message to acknowledge registration needed
-                # flash('Log in complete', 'success')
+        if username_input == user_result:
+            logging.info('Usernames matched')
+            # Start a session using username
+            # session [logged_in] = True
+            # session ['username'] = username_input
 
-                # End session
-                # session.pop('username', None)
-                return redirect(url_for('user_account'))
-            else:
-                error = 'Invalid username'
-                logging.info('No user by that username')
-                return render_template('login_user.html', error=error)
+            # Message to acknowledge registration successful
+            # flash('Log in complete', 'success')
+
+            return redirect(url_for('user_account'))
         else:
-            logging.info('No user account')
+            logging.info('No user registered under that username')
+            return render_template('login_user.html')
+            # End session
+            # session.clear('username', None)
     return render_template('login_user.html')
+
+
+# Check user is logged in - decorator
+def user_logged_in(f):
+    @wraps(f)
+    def decorator(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Unauthorized, Please login', 'danger')
+            return redirect(url_for('login_user'))
+    return decorator
+
 
 # Log out
 # @app.route('/logout')
+# @user_logged_in
 # def logout():
     # Remove username from the session
-    # session.pop('username', None)
+    # session.clear('username', None)
     # flash('You are now logged out', 'success')
     # return redirect(url_for('login_user'))
 
 # User Account
-@app.route('/user_account/<account_name>', methods=['GET', 'POST'])
-def user_account(account_name):
+@app.route('/user_account')
+@user_logged_in
+def user_account():
     return render_template("user_account.html")
 
 # Search Plants
 @app.route('/search_plants')
+@user_logged_in
 def search_plants():
     return render_template("search_plants.html", plants=mongo.db.plants.find())
 
 # Plant Record
-@app.route('/plant_record', methods=['GET', 'POST'])
+@app.route('/plant_record')
+@user_logged_in
 def plant_record():
     return render_template("plant_record.html")
 
 # Edit User Plant Record
-@app.route('/edit_user_plant_record', methods=['GET', 'POST'])
+@app.route('/edit_user_plant_record')
+@user_logged_in
 def edit_user_plant_record():
     return render_template("edit_user_plant_record.html")
 

29e63078a204e72f86c760927473c62601efdf7b
Liz94688 <elizabeth.stephens@hotmail.co.uk>
2020-04-13 10:52:33 +0000

    Started working on user_account template. Displaying correct user_plant_record information

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 # Importing Modules
 import os
 import logging
-from datetime import datetime
 from flask import (
     Flask, render_template, flash, redirect, request, url_for, session)
 from flask_pymongo import PyMongo
@@ -64,7 +63,8 @@ def register_user():
             'password': request.form['password'].lower()
         })
         # Message to acknowledge registration needed
-        flash(f'User account successfully registered for {form.username.data}', 'success')
+        flash(
+            f'User account successfully registered for {form.username.data}', 'success')
 
         return redirect(url_for('login_user'))
     return render_template('register_user.html', title='Register', form=form)
@@ -117,6 +117,11 @@ def user_logged_in(f):
     return decorator
 
 
+@user_logged_in
+def get_authenticated_user():
+    return session['username']
+
+
 # Log user out
 @app.route('/logout')
 @user_logged_in
@@ -130,11 +135,13 @@ def logout():
 
 
 # User Account
-@app.route('/user_account')
+@app.route('/user_account', methods=["GET", "POST"])
 @user_logged_in
 def user_account():
     plants = mongo.db.plants.find()
-    return render_template("user_account.html", title='User Account', plants=plants)
+    user = get_authenticated_user()
+    records = mongo.db.users_plant_records.find()
+    return render_template("user_account.html", title='User Account', plants=plants, records=records)
 
 
 # Add Plant Record Form Class
@@ -144,28 +151,38 @@ class AddPlantRecord(Form):
     notes_added = TextAreaField('Notes')
 
 # Add Plant Record
-@app.route('/add_plant_record', methods=['GET', 'POST'])
+@app.route('/add_plant_record/<plant_id>', methods=['GET', 'POST'])
 @user_logged_in
-def add_plant_record():
-    plants = mongo.db.plants.find()
-    # plant = mongo.db.plants.find_one({"_id": ObjectId(plant_id)})
-    form = AddPlantRecord(request.form)
-    users_plant_records = mongo.db.users_plant_records
+def add_plant_record(plant_id):
+    plant = mongo.db.plants.find_one({"_id": ObjectId(plant_id)})
+    # print(plant)
+    # import pdb
+    # pdb.set_trace()
     if request.method == 'POST':
+        users_plant_records = mongo.db.users_plant_records
         users_plant_records.insert({
+            'user': get_authenticated_user(),
+            'plant': {
+                '_id': plant['_id'],
+                'plant_reference': plant['plant_reference'],
+                'plant_name': plant['plant_name']
+            },
             'date_purchased': request.form['date_purchased'].lower(),
             'water_frequency': request.form['water_frequency'].lower(),
             'notes_added': request.form['notes_added'].lower()
         })
         return redirect(url_for('user_account'))
-    return render_template("add_plant_record.html", title='Add Plant Record', plants=plants, form=form)
+    else:
+        form = AddPlantRecord(request.form)
+    return render_template("add_plant_record.html", title='Add Plant Record', plant=plant, form=form)
 
 
 # Edit User Plant Record
-@app.route('/edit_user_plant_record')
+@app.route('/edit_user_plant_record', methods=["GET", "POST"])
 @user_logged_in
 def edit_user_plant_record():
-    return render_template("edit_user_plant_record.html", title='Edit Plant Record')
+    records = mongo.db.users_plant_records.find()
+    return render_template("edit_user_plant_record.html", title='Edit Plant Record', records=records)
 
 
 # Delete User Plant Record
